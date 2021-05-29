2068256ed14d5a356003e5b1fec86c099a4d3577
Matheus <matheusalexandre.sena99@gmail.com>
2021-05-28 23:08:48 -0300

    chore: finalizando layout basico

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native'
+
+export const Button = styled.Text`
+  font-size: 16px;
+  color: #006D35;
+  font-weight: bold;
+  text-transform: uppercase;
+  margin: 12px 0;
+`
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,26 +5,39 @@ import { Card } from '../components/Card'
 import { Container } from '../components/Container'
 import { Title } from '../components/Title'
 import { Row } from '../components/Row'
+import { Button } from '../components/Button'
 
 const largura = Dimensions.get('window').width
+const altura = Dimensions.get('window').height
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Container>
-        
         <Title>Próximas aulas</Title>
         <Row style={{ width: largura }}>
-          <ScrollView 
-            style={{flex: 1}} 
-            contentContainerStyle={{ width: ((largura * 0.8) * 2) + 16}} 
+          <ScrollView
+            style={{ flex: 1 }}
+            contentContainerStyle={{ width: ((largura) * 2) - 72 }}
             horizontal={true}
             showsHorizontalScrollIndicator={false} >
-              <Card style={{width: largura * 0.75}} />
-              <Card style={{width: largura * 0.75}} />
-            </ScrollView>
-          </Row>
-         
+            <Card style={{ width: largura - 64 }} />
+            <Card style={{ width: largura - 64 }} />
+          </ScrollView>
+        </Row>
+
+        <Title>Calendário acadêmico</Title>
+        <Card style={{ width: largura - 64, height: altura * 0.25 }} />
+
+        <Title>Comunidade</Title>
+        <Text>
+          Conheça pessoas, tire dúvidas, interaja com a
+          comunidade da UFABC.
+        </Text>
+        <Button>
+          veja mais
+        </Button>
+
         <StatusBar style="auto" />
       </Container>
     </SafeAreaView>
