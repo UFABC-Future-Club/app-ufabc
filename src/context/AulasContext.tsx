22f53bdaae341fab96be86191f62e0f2b8079b2d
Matheus <matheusalexandre.sena99@gmail.com>
2021-06-10 21:34:08 -0300

    chore: salvando aulas no async storage
    Co-authored-by: Henriqueorcatti <Henriqueorcatti@users.noreply.github.com>

--- src/context/AulasContext.tsx
+++ src/context/AulasContext.tsx
@@ -1,18 +1,27 @@
 import React, { useState, createContext } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Aula } from '../pages/Aulas'
 
 const defaultAulas: Aula[] = []
 
 export const CurrentAulas = createContext({
   aulas: defaultAulas,
-  editAulas: (parametro: Aula[]) => null
+  editAulas: async (parametro: Aula[]) => null
 })
 
 export default function AulasContext({ children }: { children: any }) {
   const [aulas, setAulas] = useState<Aula[]>([])
 
-  const editAulas = (aula: Aula[]) => {
+  const editAulas = async (aula: Aula[]) => {
     setAulas(aula)
+    /// 
+    try {
+      const jsonValue = JSON.stringify(aula)
+      await AsyncStorage.setItem('@storage_aula', jsonValue)
+    } catch (e) {
+      // saving error
+      throw new Error(e)
+    }
 
     return null
   }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, ScrollView, FlatList, Dimensions } from 'react-native';
 import { Card } from '../components/Card'
 import { Container } from '../components/Container'
@@ -7,12 +7,26 @@ import { Title } from '../components/Title'
 import { Row } from '../components/Row'
 import { Button } from '../components/Button'
 import { CurrentAulas } from '../context/AulasContext'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Aula } from './Aulas';
 
 export const largura = Dimensions.get('window').width
 export const altura = Dimensions.get('window').height
 
 export default function App() {
-  const { aulas } = useContext(CurrentAulas);
+  const { aulas, editAulas } = useContext(CurrentAulas);
+
+  useEffect(() => {
+    // logica
+    const obterAulas = async () => {
+      const aulasSalvas = await AsyncStorage.getItem('@storage_aula') || ""
+      const jsonAulas: Aula[] = JSON.parse(aulasSalvas)
+
+      editAulas(jsonAulas)
+    }
+
+    obterAulas()
+  }, [])
 
   return (
     <SafeAreaView style={styles.container}>
