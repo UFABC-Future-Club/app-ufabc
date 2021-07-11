4f7d279ea19c49a021a2138884bf7538e7a0f746
Matheus <matheusalexandre.sena99@gmail.com>
2021-07-10 21:05:43 -0300

    refactor: refatorando função separarAula

--- src/usecases/aulas.ts
+++ src/usecases/aulas.ts
@@ -9,11 +9,7 @@ function diaSemana(parametro: string): number {
 }
 
 function semanaAtual(): number {
-  const dt2 = new Date();
-  // yyyy-mm-dd
-  const dt1 = new Date('2021-05-25');
-
-  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  let diff = (new Date().getTime() - new Date('2021-05-25').getTime()) / 1000;
   diff /= (60 * 60 * 24 * 7);
   return Math.floor(Math.abs(diff)) + 1;
 }
@@ -21,37 +17,24 @@ function semanaAtual(): number {
 export function separarAulas(aulas: Aula[]): string[] {
   const apenasAulas = aulas.map((a) => [...a.teoria.filter(e => e !== ''), ...a.pratica.filter(e => e !== '')])
 
-  let quinzenal1: string[][] = [[], [], [], [], [], [], []]
-  let quinzenal2: string[][] = [[], [], [], [], [], [], []]
+  const indiceDiaSemana = new Date().getDay();
+  const numeroSemana = semanaAtual()
+
+  let regexQuinzena: RegExp = /quinzenal I$/gi
+
+  if (numeroSemana % 2 == 0) {
+    regexQuinzena = /quinzenal II$/gi
+  }
+
+  let quinzenal: string[][] = [[], [], [], [], [], [], []]
 
   apenasAulas.map(aula => {
-    const q1 = /quinzenal I$/gi
-    const q2 = /quinzenal II$/gi
-
     aula.map((a) => {
-      // q1, q2, semanal
-      if (q1.test(a)) {
-        // adc vetor q1
-        quinzenal1[diaSemana(a)].push(a)
-      } else if (q2.test(a)) {
-        // adc vetor q2
-        quinzenal2[diaSemana(a)].push(a)
-      } else {
-        // adc aos dois vetores
-        quinzenal1[diaSemana(a)].push(a)
-        quinzenal2[diaSemana(a)].push(a)
+      if (regexQuinzena.test(a) || /semanal$/gi.test(a)) {
+        quinzenal[diaSemana(a)].push(a)
       }
     })
   })
 
-  const indice = new Date().getDay();
-  // Quinzenal
-  const semana = semanaAtual()
-
-  if (semana % 2 == 0) {
-    return quinzenal2[indice]
-  }
-
-  return quinzenal1[indice]
+  return quinzenal[indiceDiaSemana]
 }
-
